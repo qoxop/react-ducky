24db98cf81b240eba7b332bf98834b722b293d63
张集伟 <1106291186@qq.com>
2022-06-21 11:37:52 +0800

    新增发布脚本

---
+++ scripts/release.js
@@ -0,0 +1,22 @@
+/**
+ * update api doc
+ */
+ const fs = require('fs-extra');
+ const path = require('path');
+ const { spawnSync } = require('child_process');
+ const { filesTransfer } = require('./utils');
+ 
+ const PACKAGE_FOLDER = path.resolve(__dirname, '../package');
+ const RELEASE_FOLDER = path.resolve(__dirname, '../package/lib');
+ const README_MD = path.resolve(__dirname, '../README.md');
+ const ApiDocTemp = path.resolve(PACKAGE_FOLDER, './.api-doc');
+
+ (async function () {
+   // build package
+   spawnSync('pnpm', ['build'], { cwd: PACKAGE_FOLDER, stdio: 'inherit' });
+   await fs.remove(ApiDocTemp);
+   await fs.copyFile(README_MD, path.resolve(RELEASE_FOLDER, './README.md'));
+   spawnSync('npm', ['publish'], { cwd: RELEASE_FOLDER, stdio: 'inherit' })
+ })();
+ 
+ 
