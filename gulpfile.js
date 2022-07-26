12e2487a6cb8d1fdb603b81299c8581b30d978d0
张集伟 <1106291186@qq.com>
2022-07-26 13:07:57 +0800

    增加本地包发布调试功能

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const got = require('got');
 const del = require('del');
 const path = require('path');
 const { src, dest, series } = require('gulp');
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 const { bundle, createTypes } = require('./scripts/bundle');
 
 const sourceGlobs = [
@@ -14,6 +15,19 @@ const sourceGlobs = [
 const dist = path.resolve(__dirname, './libs');
 const source = path.resolve(__dirname, './packages');
 const tsConfigPath = path.resolve(__dirname, './tsconfig.json');
+
+async function libsVisitor(cb) {
+  const dirs = fs.readdirSync(dist);
+  const pkgs = dirs.reduce((pre, cur) => {
+    const pkgPath = path.resolve(dist, cur, 'package.json')
+    const pkgInfo = require(pkgPath);
+    pre[pkgInfo.name] = { pkgInfo, pkgPath };
+    return pre;
+  }, {});
+  return await Promise.all(Object.entries(pkgs).map(async ([name, pkg]) => {
+     await cb(name, pkg, pkgs);
+  }));
+}
 
 /**
  * 删除输出目录
@@ -56,14 +70,8 @@ async function updatePackge() {
   await Promise.all(dirs.map(dir => {
     return del(path.resolve(dist, dir, '__test__'));
   }));
-  const pkgs = dirs.reduce((pre, cur) => {
-    const pkgPath = path.resolve(dist, cur, 'package.json')
-    const pkgInfo = require(pkgPath);
-    pre[pkgInfo.name] = { pkgInfo, pkgPath };
-    return pre;
-  }, {});
   // update package.json's dependencies
-  await Promise.all(Object.entries(pkgs).map(([name, pkg]) => {
+  await libsVisitor((name, pkg, pkgs) => {
     const { pkgInfo, pkgPath } = pkg;
     if (pkgInfo.dependencies) {
       Object.keys(pkgInfo.dependencies).forEach((dep) => {
@@ -85,7 +93,7 @@ async function updatePackge() {
       pkgPath,
       JSON.stringify(pkgInfo, null, '\t')
     )
-  }));
+  });
 }
 
 /**
@@ -102,26 +110,88 @@ function releasePackge(cb) {
   cb();
 }
 
-const build = series(
-  delOutput,
-  tsCompile,
-  copyPackge,
-  updatePackge
-);
+/**
+ * 发布本地包
+ */
+async function releaseLocalPackage() {
+  const { latest } = await got('http://my-npm.com/-/verdaccio/data/sidebar/react-ducky').json();
+  const newVersion = latest.version
+    .split('.')
+    .map((v, index) => (index === 2 ? +v+1 : v))
+    .join('.');
+  await libsVisitor((name, pkg, pkgs) => {
+    const { pkgInfo, pkgPath } = pkg;
+    pkgInfo.version = newVersion;
+    if (pkgInfo.dependencies) {
+      Object.keys(pkgInfo.dependencies).forEach((dep) => {
+        if (pkgs[dep]) {
+          // replace version
+          pkgInfo.dependencies[dep] = newVersion;
+          // merge peerDependencies
+          if (pkgs[dep].pkgInfo.peerDependencies) {
+            pkgInfo.peerDependencies = {
+              ...pkgs[dep].pkgInfo.peerDependencies,
+              ...pkgInfo.peerDependencies,
+            }
+          }
+        }
+      })
+    }
+    // upate file
+    return fs.promises.writeFile(
+      pkgPath,
+      JSON.stringify(pkgInfo, null, '\t')
+    )
+  });
+  const dirs = fs.readdirSync(dist).map(dir => {
+    const abs = path.resolve(dist, dir);
+    if (fs.statSync(abs).isDirectory()) {
+      return abs;
+    }
+    return '';
+  }).filter(Boolean);
+  dirs.forEach(dir => {
+    console.log(dir)
+    spawnSync('npm', ['publish', '--registry=http://my-npm.com/'], { stdio: 'inherit', cwd: dir });
+  });
+}
 
+
+/**
+ * 测试
+ */
 const test = series(
   delOutput,
   testPackage,
 );
 
-const release = series(
+/**
+ * 构建
+ */
+const build = series(
   test,
   tsCompile,
   copyPackge,
-  updatePackge,
+  updatePackge
+);
+
+/**
+ * 发布正式包
+ */
+const release = series(
+  build,
   releasePackge,
 );
+
+/**
+ * 发布本地包
+ */
+const releaseLocal = series(
+  build,
+  releaseLocalPackage
+)
 
 exports.test = test;
 exports.build = build;
 exports.release = release;
+exports.releaseLocal = releaseLocal;
